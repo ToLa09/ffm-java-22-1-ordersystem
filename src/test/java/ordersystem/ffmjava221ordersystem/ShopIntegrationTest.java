b807f50e717fb6f014773eceec6621ab4be8e416
ToLa09 <landgraf-tobias@web.de>
2022-10-12 13:00:04 +0200

    test: add Integration test for addProduct() method

--- src/test/java/ordersystem/ffmjava221ordersystem/ShopIntegrationTest.java
+++ src/test/java/ordersystem/ffmjava221ordersystem/ShopIntegrationTest.java
@@ -1,7 +1,7 @@
 package ordersystem.ffmjava221ordersystem;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ordersystem.ffmjava221ordersystem.shop.model.ProductToAdd;
+import ordersystem.ffmjava221ordersystem.shop.model.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -37,6 +38,19 @@ class ShopIntegrationTest {
                                 .andExpect(status().isOk())
                                 .andReturn().getResponse().getContentAsString();
 
-        ProductToAdd Product = objectMapper.readValue(content, ProductToAdd.class);
+        Product product = objectMapper.readValue(content, Product.class);
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products/"+product.id()))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    {
+                        "id": "<id>",
+                        "productNumber": 11,
+                        "name": "Eis",
+                        "price": 4.99
+                    }
+                    """.replace("<id>",product.id())));
+                //.andExpect(content().json("{ \"id\": "<id>", \"productNumber\": 11, \"name\": \"Eis\", \"price\": 4.99 } "));
     }
 }
